7ce32585a3ad34d0b1773d41178f00c6a60bf6a9
ShashidarBurla <burlashashidar@gmail.com>
2026-07-08 18:16:36 +0530

    shipping dns changed

--- shipping.sh
+++ shipping.sh
@@ -7,8 +7,8 @@ rm -rf /app
 mkdir -p /app
 cd /app
 unzip /tmp/shipping.zip
-mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 < db/schema.sql
-mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 < db/app-user.sql
+mysql -h mysql.shashidevops.online -u root -pRoboShop@1 < db/schema.sql
+mysql -h mysql.shashidevops.online -u root -pRoboShop@1 < db/app-user.sql
 
 useradd -r -s /bin/false appuser
 cd /app
